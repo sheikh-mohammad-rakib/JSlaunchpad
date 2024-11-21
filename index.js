2ee632c22d8e67116bdcd1aea9b5bf6073fb0f90
Sheikh Mohammad Rakib <u2009023@student.cuet.ac.bd>
2024-11-21 18:43:34 +0000

    done

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import { generateTextAndImage } from "./utils.js"
 
 // 1. Change the value of the variable to your name
-let name = "Guil Hernandez"
+let name = "Sheikh Mohammad Rakib"
 
 // 2. Change the value of the variable to your favorite activity
-let favoriteActivity = "snacking"
+let favoriteActivity = "kayaking"
 
 // 3. Assign the favoritePlace variable your favorite place
 // I.e. city, mountain, pub, forrest, beach, Manhattan, etc.
-let favoritePlace = "coffee shop"
+let favoritePlace = "florida keys"
 
 // 4. Configure the AI by setting a temperature from 0 to 1
 // The higher temperature, the more random & experimental output
-let temperature = 0.6
+let temperature = 0.7
 
 // Optional: delete "avatar.jpg" and add a photo of yourself
 // (remember to use "avatar.jpg" as the name of your photo)
